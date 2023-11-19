c5008ffa7fd4f76ccd64d7f73aaac57470b9c8fe
Nguyen Phuong <phuong.nguyen091@hcmut.edu.vn>
2023-11-19 18:31:52 +0700

    init source code

---
+++ src/Components/Navbar.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCommentDots,
+  faBars,
+  faXmark,
+  faSearch
+} from "@fortawesome/free-solid-svg-icons";
+import "../Styles/Navbar.css";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function Navbar() {
+  const [nav, setNav] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const openNav = () => {
+    setNav(!nav);
+  };
+ 
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    console.log("Searching for:", searchValue);
+  };
+
+  return (
+    <div className="navbar-section">
+      <h1 className="navbar-title">
+        <Link to="/">
+          Health <span className="navbar-sign">+</span>
+        </Link>
+      </h1>
+
+      {/* Desktop */}
+      <ul className="navbar-items">
+        <li>
+          <Link to="/" className="navbar-links">
+            Home
+          </Link>
+        </li>
+        <li>
+          <a href="#services" className="navbar-links">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="navbar-links">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#reviews" className="navbar-links">
+            Reviews
+          </a>
+        </li>
+        <li>
+          <a href="#doctors" className="navbar-links">
+            Doctors
+          </a>
+        </li>
+      </ul>
+
+      {/* <button
+        className="navbar-btn"
+        type="button"
+        disabled={isButtonDisabled}
+        onClick={handleChatBtnClick}
+      >
+        <FontAwesomeIcon icon={faCommentDots} /> Live Chat
+      </button> */}
+      <div className="navbar-search">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchValue}
+            onChange={handleSearchChange}
+            className="navbar-search-input"
+          />
+          <button
+            className="navbar-search-button"
+            type="button"
+            onClick={handleSearchSubmit}
+          >
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+      </div>
+      
+
+      {/* Mobile */}
+      <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+        <div onClick={openNav} className="mobile-navbar-close">
+          <FontAwesomeIcon icon={faXmark} className="hamb-icon" />
+        </div>
+
+        <ul className="mobile-navbar-links">
+          <li>
+            <Link onClick={openNav} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <a onClick={openNav} href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#reviews">
+              Reviews
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#doctors">
+              Doctors
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className="mobile-nav">
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={openNav}
+          className="hamb-icon"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
